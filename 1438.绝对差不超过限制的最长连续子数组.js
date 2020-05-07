bfc6885819aef75d8ec7bab72943d71154c09e58
WuChengran <chengran.wu@56qq.com>
2020-05-07 18:42:48 +0800

    feat: add q

---
+++ 1438.绝对差不超过限制的最长连续子数组.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1438 lang=javascript
+ *
+ * [1438] 绝对差不超过限制的最长连续子数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var longestSubarray = function(nums, limit) {
+
+};
+// @lc code=end
+
