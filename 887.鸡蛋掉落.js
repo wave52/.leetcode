32a0ca1abebade2bbd8d7283d02573e951a961b1
WuChengran <chengran.wu@56qq.com>
2020-06-16 11:28:56 +0800

    add

--- 887.鸡蛋掉落.js
+++ 887.鸡蛋掉落.js
@@ -10,30 +10,45 @@
  * @param {number} N
  * @return {number}
  */
-var superEggDrop = function(K, N) {
-  // dp table i K+1,鸡蛋
-  let dp = Array.from({ length: K+1 }, () => new Array(N+1).fill(0))
+var superEggDrop = function (K, N) {
+  // 备忘录
+  var memo = {}
+  // dp
+  function dp(K, N) {
+    // base case
+    if (K === 1) return N
+    if (N === 0) return 0
 
-  // base case
-  for(let j=1; j<=N; j++) dp[1][j] = j
-  for(let i=1; i<=K; i++) dp[i][0] = 0
+    if(memo[`${K}-${N}`]) return memo[`${K}-${N}`]
 
-  console.log(dp)
+    // 要用二分法，线性暴力求过不了
+    let lo = 1;
+    let hi = N;
+    let res = Infinity;
+    while (lo <= hi) {
+      const mid = Math.floor((lo + hi) / 2)
+      const broken = dp(K - 1, mid - 1) // 碎
+      const not_broken = dp(K, N - mid) // 没碎
+        // res = Math.min(Math.max(broken，not_broken) + 1) // Time Limit Exceeded
+      if (broken > not_broken) {
+        hi = mid - 1
+        res = Math.min(res, broken + 1)
+      } else {
+        lo = mid + 1
+        res = Math.min(res, not_broken + 1)
+      }
 
-  for(let i = 1; i <= K; i++) {
-    for(let j = 1; j <= N; j++) {
-      // 状态转移
-      dp[i][j] = Math.min(dp[i][j], Math.max(dp[i-1][j-1], dp[i][j-1])+1)
     }
+
+    memo[`${K}-${N}`] = res
+    return res
   }
 
-  console.log(dp)
-
-  return dp[K][N]
+  return dp(K, N)
 };
 
 // console.log(superEggDrop(1, 2))
-console.log(superEggDrop(2, 6))
+console.log(superEggDrop(4, 5000))
 
 // @lc code=end
 
