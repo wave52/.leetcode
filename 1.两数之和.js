ff093cba523ce2c093b14b37db300a0af086c414
WuChengran <chengran.wu@56qq.com>
2020-06-30 11:34:21 +0800

    add

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -9,6 +9,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j]
+      }
+  }
+  }
 };
 
