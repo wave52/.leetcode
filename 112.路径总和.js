10c82dd719015a2e8c722a20782beabec0a48fc4
WuChengran <chengran.wu@56qq.com>
2020-07-07 14:32:20 +0800

    add

---
+++ 112.路径总和.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=javascript
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, sum) {
+  if(root === null) return false
+  const leave = sum - root.val
+  if (leave === 0 && !root.left && !root.right) {
+    return true
+  } else {
+    return hasPathSum(root.left, leave) || hasPathSum(root.right, leave)
+  }
+};
+// @lc code=end
+
